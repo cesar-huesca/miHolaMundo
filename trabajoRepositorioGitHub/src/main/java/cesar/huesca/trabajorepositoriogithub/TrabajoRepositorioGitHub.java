28274d51054a7ac0951a4e89c6e07f394f8c4864
pepe <pepe@alumno>
2022-04-06 17:33:43 +0200

    COMMIT PEPE PRINTLN

--- trabajoRepositorioGitHub/src/main/java/cesar/huesca/trabajorepositoriogithub/TrabajoRepositorioGitHub.java
+++ trabajoRepositorioGitHub/src/main/java/cesar/huesca/trabajorepositoriogithub/TrabajoRepositorioGitHub.java
@@ -13,5 +13,6 @@ public class TrabajoRepositorioGitHub {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("SOY PEPE");
     }
 }
